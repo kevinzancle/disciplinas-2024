9474758225523626ed7dc42b37974d3676eb507d
Kevin Zancle <kzancle@gmail.com>
2024-06-26 17:29:27 -0300

    add aula 19

---
+++ repo-uc13/AulaForWhile/app/src/main/java/com/example/aulaforwhile/MainActivity2While.java
@@ -0,0 +1,35 @@
+package com.example.aulaforwhile;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2While extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_activity2_while);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void acionar2 (View view){
+        int i = 0;
+        while(i<10){
+            i++;
+        }
+        Toast.makeText(this, "LANÇAMENTO CONCLUÍDO!", Toast.LENGTH_LONG).show();
+    }
+
+}
