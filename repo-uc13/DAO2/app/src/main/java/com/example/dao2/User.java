5fcc9a802d2d29976546b744d3056c45dfef354c
kevinzancle <163158286+kevinzancle@users.noreply.github.com>
2024-10-24 10:19:50 -0300

    add= DAO2

---
+++ repo-uc13/DAO2/app/src/main/java/com/example/dao2/User.java
@@ -0,0 +1,42 @@
+package com.example.dao2;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
